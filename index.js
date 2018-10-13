92e22edff4a24bc8e427ad3e77c713a0df0a5bee
Carol Greene <carolgreene3@gmail.com>
2018-10-13 22:12:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,5 +15,8 @@ function calculatesFarePrice(start, destination) {
   let distance = distanceTravelledInFeet(start, destination)
     if (distance <= 400) {
     return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return (distance - 400) * .02;
+
   }
 }
