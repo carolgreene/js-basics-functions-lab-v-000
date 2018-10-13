1600cf3f34ff9c61772ba3c9a18672845294b0cb
Carol Greene <carolgreene3@gmail.com>
2018-10-13 21:58:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function calculatesFarePrice(start, destination) {
     if (distance <= 400) {
     return 0;
   } else if (distance > 400 && distance <= 2000) {
-    return distance * .02;
+    return (distance - 400) * .02;
 
 
   }
