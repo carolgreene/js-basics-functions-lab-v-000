09e8d60146b33d0a49a14aba12c6ed2f91d2c8db
Carol Greene <carolgreene3@gmail.com>
2018-10-13 21:46:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,20 @@ function distanceFromHqInFeet(destination) {
   return (distanceFromHqInBlocks(destination) * 264)
 }
 
-function distanceTravelledInFeet(start, end) {
-  return Math.abs((start - end) * 264)
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs((start - destination) * 264)
+}
+
+function calculatesFarePrice(start, destination) {
+  switch (fare) {
+    case distanceTravelledInFeet <= 400:
+    return 0;
+    case distanceTravelledInFeet > 400 && distanceTravelledInFeet <= 2000:
+    return distanceTravelledInFeet * .02;
+    case distanceTravelledInFeet > 2000 && distanceTravelledInFeet <= 2500:
+    return 25;
+    case distanceTravelledInFeet > 2500:
+    return 'cannot travel that far'
+
+  }
 }
